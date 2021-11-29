22f2d70135fa5b74bc3ed12aeda4ca9f2834773b
Richard Willis <rwillis@shaw.ca>
2021-11-29 13:29:53 -0800

    [Octoprint-Plugin] Tutorial Plugin. See README.

---
+++ octoprint_helloworld/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import octoprint.plugin
+
+class HelloWorldPlugin( octoprint.plugin.StartupPlugin, 
+                        octoprint.plugin.TemplatePlugin,
+                        octoprint.plugin.SettingsPlugin):
+    def on_after_startup( self ):
+        self._logger.info( "Hello World (more: %s)" % self._settings.get(["url"]) )
+        
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello World")
+
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False)
+        ]
+
+
+__plugin_name__ = "Hello World"
+__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_implementation__ = HelloWorldPlugin()
+
